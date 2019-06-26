8446eda744987005c37bdea2477948175e1747ef
kendirlialper <alper_kendirli@hotmail.com>
2019-06-26 20:53:21 +0300

    Update LSDB.java

--- src/DB/LSDB.java
+++ src/DB/LSDB.java
@@ -11,10 +11,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author KayademirS
- */
+
 public class LSDB {
 
     //veritabanına baglantı sağlıyor
